4cbf2968e6ebcfd75853596d9a504a876d57fc7a
kuk329 <kuk329@cau.ac.kr>
2022-01-04 22:13:27 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6)+1; // 1~6까지 숫자 랜덤 생성
+                  print('diceNumber = $leftDiceNumber');
+                });
+
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print("Right button got  pressed");
+              },
+              child: Image.asset('images/dice3.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
